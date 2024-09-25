7afae611aab7d0689e1ba3a31d2b20757bfa2537
samuel.jimenez <samuel.jimenez@106-15.info2>
2024-09-25 14:10:22 +0200

    Update

--- Tema1/src/parte1/Ejer15.java
+++ Tema1/src/parte1/Ejer15.java
@@ -5,18 +5,7 @@ import java.util.Scanner;
 public class Ejer15 {
 
 	public static void main(String[] args) {
-		int nota1º;
-		int  nota2º;
-		int nota3;
-		double calculo;
-Scanner lectura= new Scanner(System.in);
-System.out.println("Introduce tus notas del primer segundo y tercer trimestre en ese orden para calcular la media goblar");
-nota1º=lectura.nextInt();
-nota2º=lectura.nextInt();
-nota3=lectura.nextInt();
-calculo= (nota1º+nota2º+nota3)/3;
-System.out.println("La nota en el boletin sera un"+ " " + calculo );
-System.out.println("La nota en el expediente sera un" + " " + (double) calculo );
+
 
 	}
 
